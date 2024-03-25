79c6e02bab1d4c642fedb02e041366932c8325e0
librarianmage <raymond.arndorfer@gmail.com>
2024-03-24 21:33:10 -0500

    Add Gilda the lost by Aury System

---
+++ Gilda the lost/AUWreckedMechaGildaBuilder.cs
@@ -0,0 +1,43 @@
+﻿
+using System.Linq;
+using Genkit;
+using XRL.World;
+using XRL;
+
+
+namespace XRL.World.ZoneBuilders
+{
+    public class AUWreckedMechaGildaBuilder : ZoneBuilderSandbox
+    {
+        public bool BuildZone(Zone zone)
+        {
+            InfluenceMapRegion influenceMapRegion = ZoneBuilderSandbox.GenerateInfluenceMap(zone, null, InfluenceMapSeedStrategy.LargestRegion, 200).
+                Regions.Where((InfluenceMapRegion r) => r.maxRect.Width >= 19 && r.maxRect.Height >= 19).FirstOrDefault();
+            Cell cell = null;
+            if (influenceMapRegion != null)
+            {
+                cell = zone.GetCell(influenceMapRegion.maxRect.x1, influenceMapRegion.maxRect.y1);
+            }
+            if (cell == null)
+            {
+                cell = (from c in zone.GetCells()
+                        where c.X < 58 && c.Y < 19
+                        select c).GetRandomElement();
+            }
+            for (int y = 0; y < 19; y++)
+            {
+                for (int x = 0; x < 19; x++)
+                {
+                    zone.GetCell(cell.X + x, cell.Y + y)?.Clear(null, Important: false, Combat: true);
+                }
+            }
+            cell.AddObject("AUWreckedMecha-GildaLost");
+
+            ZoneTemplateManager
+                .Templates["AUWreckedMecha-LostEnclave"]
+                .Execute(zone);
+
+            return true;
+        }
+    }
+}
---
+++ Gilda the lost/AUWreckedMechaWorldPlacement.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using XRL.World;
+using XRL.World.Parts;
+using XRL.World.ZoneBuilders;
+using Genkit;
+
+namespace XRL.World.WorldBuilders
+{
+    [JoppaWorldBuilderExtension]
+    public class AUWreckedMechaWorldPlacement : IJoppaWorldBuilderExtension
+    {
+        public override void OnAfterBuild(JoppaWorldBuilder builder)
+        {
+            Location2D location = builder.mutableMap.popMutableLocationInArea(228, 6, 233, 11);
+
+            string zoneID = Zone.XYToID("JoppaWorld", location.X, location.Y, 10);
+
+            The.ZoneManager.AddZoneBuilder(zoneID, ZoneBuilderPriority.AFTER_TERRAIN, nameof(AUWreckedMechaGildaBuilder));
+
+            string secret = builder.AddSecret(zoneID, "a lost enclave of Barathrumites", new string[4] { "encounter", "special", "settlement", "barathrumites" }, "Settlements", "$auwreckedmechalost");
+
+            if (The.ZoneManager.GetZone("JoppaWorld").GetCell(location.X / 3, location.Y / 3).GetFirstObjectWithPart("TerrainTravel")
+                .TryGetPart<TerrainTravel>(out var Part))
+            {
+                Part.AddEncounter(new EncounterEntry("You encounter strewn battle wreckage, Urshiib tinkers picking at the pieces. Would you like to investigate?", zoneID, "", secret, Optional: true));
+            }
+
+            
+            GameObject gameObject = GameObjectFactory.Factory.CreateObject("SecretRevealWidget");
+            SecretRevealer part = gameObject.GetPart<SecretRevealer>();
+            part.message = "You discover a seperated enclave of Barathrumites.";
+            part.id = secret;
+            part.adjectives = "encounter,special,settlement,barathrumites";
+            part.category = "Settlements";
+            part.text = "Lost enclave";
+            The.ZoneManager.AddZonePostBuilder(zoneID, "AddWidgetBuilder", "Object", The.ZoneManager.CacheObject(gameObject));
+
+
+        }
+    }
+}
